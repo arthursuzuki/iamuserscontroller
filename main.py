949d771503bf6a0871d7957b85d25dacfdf54c76
suzuki <ajls@cesar.school>
2024-09-10 21:40:05 -0300

    uniting users and commands functions

--- main.py
+++ main.py
@@ -1,21 +1,33 @@
 import os
 from generatepassword import generate_password
 import subprocess
+from users import createUsersException, getUsersList
 
-password = generate_password()
-print(password)
+newPassword = generate_password()
+print(newPassword)
 
-command = [
-    "aws", "iam", "update-login-profile",
-    "--user-name", f"iuctester",
-    "--password", f"{password}"
-]
+users = list(getUsersList())
+i = int(input("You have any users exeptions? (1 for yes, other number to continue )"))
+if(i==1):
+    users = createUsersException(users)
 
-# Executa o comando no CMD usando subprocess
-try:
-    result = subprocess.run(command, capture_output=True, text=True, check=True)
-    print("Comando executado com sucesso:")
-    print(result.stdout)
-except subprocess.CalledProcessError as e:
-    print(f"Erro ao executar o comando: {e}")
-    print(e.stderr)
+i = users.count()
+
+for i in users:
+    newPassword = generate_password()
+    print(newPassword)
+    command = [
+        "aws", "iam", "update-login-profile",
+        "--user-name", f"{users[i]}",
+        "--password", f"{newPassword}"
+    ]
+
+    try:
+        result = subprocess.run(command, capture_output=True, text=True, check=True)
+        print("Comando executado com sucesso:")
+        print(result.stdout)
+    except subprocess.CalledProcessError as e:
+        print(f"Erro ao executar o comando: {e}")
+        print(e.stderr)
+
+    i = i-1
