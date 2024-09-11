17729855a8b61d7138653d34e95a4a1bffd0145d
suzuki <ajls@cesar.school>
2024-09-11 09:20:05 -0300

    treating errors

--- users.py
+++ users.py
@@ -1,33 +1,42 @@
 import boto3
+from botocore.exceptions import *
 
 # Cria uma sessão com o cliente IAM
 client = boto3.client('iam')
 
 def getUsersList():
     # Chama o método list_users
-    response = client.list_users()
+    try:
+        response = client.list_users()
 
-    # Itera sobre os usuários e imprime o nome de cada um
-    for user in response['Users']:
-        print(f"UserName: {user['UserName']}, UserId: {user['UserId']}")
+        # Itera sobre os usuários e imprime o nome de cada um
+        for user in response['Users']:
+            print(f"UserName: {user['UserName']}, UserId: {user['UserId']}")
 
-    users = list()
-    for user in response['Users']:
-        users.append(user['UserName'])
-    users.remove("admin")
-    print(users)
-    return users
+        users = list()
+        for user in response['Users']:
+            users.append(user['UserName'])
+        users.remove("admin")
+        print(users)
+        return users
+    except:
+        print(Exception)
+
 
 def createUsersException(users):
-    entrada = input("Enter the users to remove (separated by space): ")
-    lista = entrada.split()
-    print(f"Usuários a serem removidos: {lista}")
-    for user in lista:
-        if user in users:
-            users.remove(user)
-            print(f"Usuário {user} removido da lista.")
-        else:
-            print(f"Usuário {user} não encontrado na lista.")
-    
-    print(f"Lista final de usuários: {users}")
+    try:
+        entrada = input("Enter the users to remove (separated by space): ")
+        lista = entrada.split()
+        print(f"Users to be removed: {lista}")
+        for user in lista:
+            if user in users:
+                users.remove(user)
+                print(f"Usuário {user} removed from list.")
+            else:
+                print(f"Usuário {user} not found.")
+        
+        print(f"Lista final de usuários: {users}")
+    except:
+        print("Error creating users exceptions")
 
+getUsersList()
